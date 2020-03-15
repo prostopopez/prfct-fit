e87538ce10494d39ca956d4605b69d6744cbf2ac
prostopopez <gtaas@mail.ru>
2020-03-15 13:06:41 +0300

    added module Card

---
+++ src/components/Card/card.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './card.css';
+
+const CardFunc = () => {
+    return(
+        <div>
+            <h2>Suck</h2>
+        </div>
+    );
+};
+
+export default CardFunc;
