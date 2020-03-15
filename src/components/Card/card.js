1189859428431334d85b5bbd0e3472e841637258
prostopopez <gtaas@mail.ru>
2020-03-15 19:47:30 +0300

    changed getData

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -1,10 +1,12 @@
 import React from "react";
+import * as getData from "../../getData";
 import './card.css';
 
 const CardFunc = () => {
     return(
         <div>
             <h2>Suck</h2>
+            <p></p>
         </div>
     );
 };
--- src/getData.js
+++ src/getData.js
@@ -1,14 +1,34 @@
 const requestURL = "http://dev-test-black.ru/testTask/data.json";
 
 function sendRequest(method, url, body = null){
-    return fetch(url);
+    return fetch(url).then(response => {
+        if(response.ok) {
+            return response.json();
+        }
+        return response.json().then( error => {
+            const e = new Error("Ошибка");
+            e.data = error;
+            throw e;
+        });
+    });
 }
 
+let serverResponse = [];
+let userData = {};
+let articleData = [];
+
 sendRequest("GET", requestURL)
-    .then(data => console.log(data))
+    .then(function(data) {
+        for(let i in Object.values(data)) {
+            serverResponse.push(Object.values(data)[i]);
+        }
+
+        for (let j in serverResponse[0]) {
+            userData[j] = serverResponse[0][j];
+        };
+
+        articleData = serverResponse[1].slice();
+    })
     .catch(error => console.log(error));
 
-const body = {
-    name: "Gleb",
-    age: 21
-};
+console.log(serverResponse, userData, articleData);
